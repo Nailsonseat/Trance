339d58d467ceaf150bbb26c16a7420ce872d9ea3
nailsonseat <aadarshv3@gmail.com>
2023-11-15 18:00:33 +0530

    added check for auth token stored

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
+import "./style.css";
 import { router } from "./router/router.js";
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== "Login" && !localStorage.getItem("auth-token") ? true : false)
+    next({ name: "Login" });
+  else next();
+});
 
 const app = createApp(App);
 app.use(router);
