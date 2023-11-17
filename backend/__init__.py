e8adf9f96dd132a4ba88b27d586c9262740325db
nailsonseat <aadarshv3@gmail.com>
2023-11-17 09:59:55 +0530

    minor changes in config

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
-CORS(app, resources={r'/*': {'origins': '*'}}, CORS_SUPPORTS_CREDENTIALS=True)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = 'f724f39fcae64ca2ba988bf09ee91c66'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECURITY_LOGIN_URL'] = '/login'
+# app.config['SECURITY_LOGIN_URL'] = '/login'
 app.config['SECURITY_TOKEN_AUTHENTICATION_HEADER'] = 'auth'
 app.config['SECURITY_TOKEN_AUTHENTICATION_KEY'] = 'Authorization'
 app.config['SECURITY_TOKEN_MAX_AGE'] = 3600
@@ -21,7 +20,7 @@ app.config['SECURITY_TOKEN_URL'] = '/token'
 app.config['SECURITY_EMAIL_DOMAIN'] = 'gmail.com'
 app.config['SECURITY_USERNAME'] = True
 app.config['SECURITY_USERNAME_REQUIRED'] = True
-app.config['WTF_CSRF_ENABLED'] = True
+app.config['WTF_CSRF_ENABLED'] = False
 app.config['SECURITY_TRACKABLE'] = True
 app.config['SECURITY_POST_LOGIN_VIEW'] = '/protected'
 app.config['SECURITY_POST_LOGOUT_VIEW'] = '/login'
@@ -32,5 +31,5 @@ app.config['SECURITY_DEFAULT_ROLES'] = 'user'
 
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-api = Api(app)
+# migrate = Migrate(app, db)
+api = Api(app, prefix="/api/v1")
