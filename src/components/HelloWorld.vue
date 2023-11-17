e7e951d19f8fc2f80e2785f919e3cbec5afd50d4
nailsonseat <aadarshv3@gmail.com>
2023-11-17 11:18:39 +0530

    test axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,26 @@
 <script setup>
 import { ref } from 'vue'
+import axios from 'axios'
 
 defineProps({
   msg: String,
 })
 
 const count = ref(0)
+
+const path = 'http://127.0.0.1:5000/dataentry'
+
+function apiTest(){
+  axios.post(path, {name:"Aadarsh Verma"})
+}
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="{count++; apiTest();}">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
