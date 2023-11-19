5bec4cbcc4474841cd57ceb458da8108488e32f7
nailsonseat <aadarshv3@gmail.com>
2023-11-19 22:46:20 +0530

    Completed desigining the user auth page

---
+++ src/components/UserAuth.vue
@@ -0,0 +1,183 @@
+<template>
+    <div class="auth-component">
+        <div class="role-switcher" style="display: flex;">
+            <button class="user-role-button">User</button>
+            <button @click="goToCreatorAuth()" class="creator-role-button">Creator</button>
+        </div>
+        <div class="login-container">
+
+            <h1>{{ isLoginPage ? 'Login' : 'Register' }}</h1>
+            <form @submit.prevent="submitForm">
+                <label class="input-label" for="username">Email</label>
+                <input class="input" type="text" id="username" v-model="email" />
+
+                <label class="input-label" for="password">Password</label>
+
+                <input class="input" type="password" id="password" v-model="password"
+                    :type="showPassword ? 'text' : 'password'" />
+
+
+                <!-- Additional fields for registration -->
+                <label class="input-label" v-if="!isLoginPage" for="confirm-password">Confirm Password</label>
+                <input class="input" v-if="!isLoginPage" id="confirm-password" v-model="confirmPassword" />
+
+                <button class="submit-button" type="submit">{{ isLoginPage ? 'Login' : 'Register' }}</button>
+            </form>
+
+            <!-- Switch to toggle between login and register forms -->
+            <div class="toggle-container">
+                <span>{{ isLoginPage ? "Don't have an account?" : "Already have an account?" }}</span>
+                <Toggle class="switcher" v-model="isLoginPage" on-label="Login" off-label="Register" />
+            </div>
+
+        </div>
+    </div>
+</template>
+
+
+<style src="@vueform/toggle/themes/default.css"></style>
+<script>
+import axios from 'axios';
+import Toggle from '@vueform/toggle'
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            confirmPassword: '',
+            isLoginPage: true, // Default to show login form
+            showPassword: false,
+        };
+    },
+    components: {
+        Toggle,
+    },
+    methods: {
+        goToCreatorAuth() {
+            this.$router.push('/login-c');
+        },
+        togglePasswordVisibility() {
+            this.showPassword = !this.showPassword;
+        },
+        async submitForm() {
+            // Add your logic for login or register based on the value of isLogin
+            if (this.isLoginPage) {
+                // Login logic
+                console.log('Logging in with:', this.email, this.password);
+            } else {
+                // Register logic
+                console.log('Registering with:', this.email, this.password, this.confirmPassword);
+            }
+
+            const path = 'http://127.0.0.1:5000/';
+            axios.post(path + 'login-user', { email: this.email, password: this.password }, { headers: { 'Access-Control-Allow-Origin': '*' } })
+                .then(response => {
+                    console.log('Operation successful:', response.data);
+                    // Redirect to the dashboard or perform other actions based on the response
+                    this.$router.push('/dashboard');
+                })
+                .catch(error => {
+                    console.error('Operation error:', error.response.data.error);
+                });
+        },
+    },
+};
+</script>
+
+<style scoped>
+.auth-component {
+    border: 1px solid #ccc;
+    border-radius: 25px;
+    max-width: 630px;
+    margin: 0 auto;
+    box-shadow: 0 0 50px rgba(255, 255, 255, 0.1);
+}
+
+.role-switcher {
+    max-width: 630px;
+    margin: 0 auto;
+    height: 80px;
+}
+
+.user-role-button {
+    flex: 1;
+    border-radius: 25px 0 0 0;
+    font-size: x-large;
+    color: #42b883;
+    background-color: #242424;
+    border: 1px solid transparent;
+}
+
+.creator-role-button {
+    flex: 1;
+    border-radius: 0 25px 0 25px;
+    font-size: x-large;
+    color: white;
+    background-color: #242424;
+    border: 1px solid;
+    border-color: transparent transparent #ccc #ccc;
+    position: relative;
+    box-shadow: 0 0 15px rgba(255, 255, 255, 0.1) inset;
+}
+
+.login-container {
+    max-width: 450px;
+    padding-top: 20px;
+    padding-bottom: 50px;
+    padding-left: 90px;
+    padding-right: 90px;
+
+}
+
+.toggle-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.input {
+    margin-bottom: 2.5rem;
+    height: 45px;
+    border-radius: 20px;
+    font-size: medium;
+    border-color: #42b883;
+    padding-left: 15px;
+}
+
+.input-label {
+    font-size: large;
+    align-self: start;
+    padding-bottom: 2px;
+}
+
+.switcher {
+    --toggle-width: 8rem;
+    --toggle-height: 2.3rem;
+    --toggle-font-size: 1rem;
+    align-items: center;
+    margin-left: 15px;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+label {
+    margin-bottom: 5px;
+}
+
+.submit-button {
+    padding: 10px;
+    height: 45px;
+    margin-bottom: 2.5rem;
+    background-color: #42b883;
+    color: white;
+    border: none;
+    cursor: pointer;
+    font-size: medium;
+}
+
+/* Switch styles */
+</style>
