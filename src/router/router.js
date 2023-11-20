fa3e839c9926e6194e99a5d4c4bb86a6d9cadea7
nailsonseat <aadarshv3@gmail.com>
2023-11-20 16:53:59 +0530

    added routes

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,13 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HelloWorldVue from "../components/HelloWorld.vue";
+import Login from "../components/Login.vue";
+import Dashboard from "../components/Dashboard.vue";
 
-const routes = [{ path: "/", component: HelloWorldVue }];
+const routes = [
+  { path: "/", component: HelloWorldVue, name: "base" },
+  { path: "/login", component: Login, name: "Login" },
+  { path: "/dashboard", component: Dashboard, name: "Dashboard" },
+];
 
 export const router = createRouter({
   history: createWebHashHistory(),
