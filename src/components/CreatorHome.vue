7ef9b30d039d86dc8230207ac833444c86eca580
nailsonseat <aadarshv3@gmail.com>
2023-11-23 23:57:22 +0530

    Added a dummy creator page

---
+++ src/components/CreatorHome.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+    methods: {
+        createSong() {
+            // Implement logic for creating a new song
+            console.log("Creating a new song");
+        },
+        editSong() {
+            // Implement logic for editing an existing song
+            console.log("Editing an existing song");
+        },
+        removeSong() {
+            // Implement logic for removing an existing song
+            console.log("Removing an existing song");
+        },
+        createAlbum() {
+            // Implement logic for creating a new album
+            console.log("Creating a new album");
+        },
+        editAlbum() {
+            // Implement logic for editing an existing album
+            console.log("Editing an existing album");
+        },
+        removeAlbum() {
+            // Implement logic for removing an existing album
+            console.log("Removing an existing album");
+        },
+        assignSongToAlbum() {
+            // Implement logic for assigning a song to an album
+            console.log("Assigning a song to an album");
+        },
+    },
+};
+</script>
+
+<template>
+    <div>
+        <h2 style="text-align: start; font-size: 45px; margin-top: 30px; margin-left: 50px;">Trance Creator Studio</h2>
+        <div class="functionality-section">
+            <h3>User Functionalities</h3>
+            <!-- Include user functionalities here -->
+            <router-link :to="{ name: 'UserAuth' }">Go to User Home</router-link>
+        </div>
+        <div class="functionality-section">
+            <h3>Creator Functionalities</h3>
+            <!-- Include creator functionalities here -->
+            <button @click="createSong">Create Song</button>
+            <button @click="editSong">Edit Song</button>
+            <button @click="removeSong">Remove Song</button>
+            <button @click="createAlbum">Create Album</button>
+            <button @click="editAlbum">Edit Album</button>
+            <button @click="removeAlbum">Remove Album</button>
+            <button @click="assignSongToAlbum">Assign Song to Album</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.functionality-section {
+    margin-top: 20px;
+}
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,7 @@ import CreatorAuth from "../components/CreatorAuth.vue";
 import AdminAuth from "../components/AdminAuth.vue";
 import Dashboard from "../components/Dashboard.vue";
 import UserHome from "../components/UserHome.vue";
+import CreatorHome from "../components/CreatorHome.vue";
 
 const routes = [
   { path: "/", component: HelloWorldVue, name: "base" },
@@ -13,6 +14,7 @@ const routes = [
   { path: "/admin-auth", component: AdminAuth, name: "AdminAuth" },
   { path: "/dashboard", component: Dashboard, name: "Dashboard" },
   { path: "/home-user", component: UserHome, name: "UserHome" },
+  { path: "/home-creator", component: CreatorHome, name: "CreatorHome" },
 ];
 
 export const router = createRouter({
