6c97d7fe8c4a058b26291534b190d9d9a673088d
nailsonseat <aadarshv3@gmail.com>
2023-11-24 17:39:20 +0530

    fixed overlapping bootstrap and custom CSS

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
+import "bootstrap/dist/css/bootstrap.css";
+
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./style.css";
+
 import { router } from "./router/router.js";
 
-router.beforeEach((to, from, next) => {
-  if (to.name !== "Login" && !localStorage.getItem("auth-token") ? true : false)
-    next({ name: "Login" });
-  else next();
-});
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import "./style.css";
+
+// router.beforeEach((to, from, next) => {
+//   if (to.name !== "UserAuth" && !localStorage.getItem("auth-token") ? true : false)
+//     next({ name: "UserAuth" });
+//   else next();
+// });
 
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
+
+import "bootstrap/dist/js/bootstrap.js";
