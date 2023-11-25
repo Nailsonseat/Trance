1255d50c9afd1a097384b68614debf970f827c13
nailsonseat <aadarshv3@gmail.com>
2023-11-26 00:50:36 +0530

    added role creator

--- backend/app.py
+++ backend/app.py
@@ -9,9 +9,11 @@ def initalize_roles():
         name='admin', description='Administrator')
     app.Security.datastore.find_or_create_role(
         name='user', description='User')
-    if not app.Security.datastore.find_user(username='Admin'):
+    app.Security.datastore.find_or_create_role(
+        name='creator', description='Creator')
+    if not app.Security.datastore.find_user(username='admin'):
         app.Security.datastore.create_user(
-            username='Admin', email='admin@gmail.com', password=hash_password('12345678'), roles=[admin_role])
+            username='admin', email='admin@gmail.com', password=hash_password('12345678'), roles=[admin_role])
 
 
 if __name__ == '__main__':
