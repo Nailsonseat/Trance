ca1a80a6c52fc06415d6bda2872312db161db6e8
nailsonseat <aadarshv3@gmail.com>
2023-11-27 11:27:55 +0530

    accomodated both login pages if auth token not present

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 import "bootstrap/dist/css/bootstrap.css";
-
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import { router } from "./router/router.js";
-
 import "bootstrap-icons/font/bootstrap-icons.css";
-
 import "./style.css";
 
-// router.beforeEach((to, from, next) => {
-//   if (to.name !== "UserAuth" && !localStorage.getItem("auth-token") ? true : false)
-//     next({ name: "UserAuth" });
-//   else next();
-// });
+const tokenKey = "auth-token";
+router.beforeEach((to, from, next) => {
+  if (
+    to.name !== "UserAuth" &&
+    to.name !== "CreatorAuth" &&
+    !localStorage.getItem("auth-token")
+  )
+    next({ name: "UserAuth" });
+  else next();
+});
 
 const app = createApp(App);
 app.use(router);
