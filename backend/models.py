822514543f8232a4f5ba98a65a3d0418f224b9d0
nailsonseat <aadarshv3@gmail.com>
2023-11-30 22:44:35 +0530

    added table for playlist

--- backend/models.py
+++ backend/models.py
@@ -68,4 +68,11 @@ class Album(db.Model):
     genre = db.Column(db.String(50))
     reports = db.Column(db.Integer, default=0)
     cover_path = db.Column(db.String(255))
+
+
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
