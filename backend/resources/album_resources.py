a8873bb99cfac0161c6c968579979bb2482b99d1
nailsonseat <aadarshv3@gmail.com>
2023-12-06 23:14:42 +0530

    added album resource to get album by album id

---
+++ backend/resources/album_resources.py
@@ -0,0 +1,10 @@
+
+
+class AlbumResource(Resource):
+    @marshal_with(album_fields)
+    def get(self, album_id):
+        album = Album.query.get_or_404(album_id)
+        # Fetch associated songs for the album
+        songs = Song.query.filter_by(album_id=album_id).all()
+        album.songs = songs
+        return album
