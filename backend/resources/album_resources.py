81dbe438bdccb67c9d93b05b5e18e19dcc8eb38d
nailsonseat <aadarshv3@gmail.com>
2023-12-06 23:25:08 +0530

    album create resource to create new album

--- backend/resources/album_resources.py
+++ backend/resources/album_resources.py
@@ -1,3 +1,9 @@
+from models import Song, Album, Playlist, PlaylistSong, SongsLiked
+from flask_restful import Resource, reqparse, marshal_with, fields
+from flask_security import roles_required, auth_required
+from models import db
+from datetime import datetime
+from resources.fields import song_fields, album_fields
 
 
 class AlbumResource(Resource):
@@ -16,3 +22,30 @@ class AlbumListResource(Resource):
         albums = Album.query.all()
         return albums
 
+
+
+class AlbumCreateResource(Resource):
+    @marshal_with(album_fields)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', type=str, required=True,
+                            help='Album title is required')
+        parser.add_argument('artist', type=str, required=True,
+                            help='Artist name is required')
+        parser.add_argument('release_date', type=str,
+                            required=True, help='Release date is required')
+        parser.add_argument('genre', type=str, required=True,
+                            help='Genre is required')
+        args = parser.parse_args()
+
+        # Logic to create a new album
+        new_album = Album(
+            title=args['title'],
+            artist=args['artist'],
+            release_date=args['release_date'],
+            genre=args['genre']
+        )
+        db.session.add(new_album)
+        db.session.commit()
+
+        return new_album, 201
