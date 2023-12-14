42caa3047b4bd6932dfd6d3cd112b6ef4aa659d2
nailsonseat <aadarshv3@gmail.com>
2023-12-14 13:04:37 +0530

    cover resource initital commit

---
+++ backend/resources/cover_resources.py
@@ -0,0 +1,33 @@
+from flask import request
+from flask_restful import Resource, reqparse
+from werkzeug import datastructures
+import os
+from datetime import datetime
+
+
+class CoverUploadResource(Resource):
+
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument(
+            'file', type=datastructures.FileStorage, location='files', required=True, help='Image file is required')
+
+    def post(self):
+        image_file = request.files['file']
+
+        if image_file:
+            # Generate a unique filename using the current timestamp
+            timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+            filename = f"{timestamp}_{image_file.filename}"
+
+            # Save the file to the assets folder
+            upload_path = os.path.join(
+                "../src/assets/cover", filename)
+            image_file.save(upload_path)
+
+            return {
+                'message': 'Cover image uploaded successfully',
+                'cover_path': upload_path
+            }, 201
+        else:
+            return {'message': 'No image file provided'}, 400
