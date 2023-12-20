e4838ecb9ca9c9ab3287814c5cdd468d9641ff1b
nailsonseat <aadarshv3@gmail.com>
2023-12-20 15:15:28 +0530

    initial song replacement resource

--- backend/resources/song_upload_resources.py
+++ backend/resources/song_upload_resources.py
@@ -65,3 +65,52 @@ class SongUploadResource(Resource):
             return make_response(jsonify(response_data), 201)
         else:
             return {'message': 'No MP3 file provided'}, 400
+
+
+class SongReplacementResource(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument(
+            'file', type=FileStorage, location='files', required=True, help='MP3 file is required')
+
+    def put(self, song_id):
+        song = Song.query.get_or_404(song_id)
+        mp3_file = request.files['file']
+
+        if mp3_file:
+            # Generate a unique filename using the current timestamp
+            timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+            filename = f"{timestamp}_{mp3_file.filename}"
+
+            # Save the file to the assets folder
+            upload_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            open(upload_path, 'w+')
+            mp3_file.save(upload_path)
+            response_data = {}
+            audio = MP3(upload_path)
+
+            # contains all the metadata about the mp3 file
+            audio_info = audio.info
+            length = int(audio_info.length)
+            hours, minutes, seconds = audio_duration(length)
+
+            # Update the song information in the database
+            song.filepath = upload_path
+            song.hours = hours
+            song.minutes = minutes
+            song.seconds = seconds
+
+            db.session.commit()
+
+            response_data = {
+                'message': 'File replaced successfully',
+                'file_path': upload_path,
+                'duration': {
+                    "hours": hours,
+                    "minutes": minutes,
+                    "seconds": seconds
+                }
+            }
+            return make_response(jsonify(response_data), 200)
+        else:
+            return {'message': 'No MP3 file provided'}, 400
