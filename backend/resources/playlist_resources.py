b9062e560ab2d295d000621d68312e516bd0c597
nailsonseat <aadarshv3@gmail.com>
2023-12-20 14:49:56 +0530

    updated create playlist post method

--- backend/resources/playlist_resources.py
+++ backend/resources/playlist_resources.py
@@ -15,39 +15,46 @@ class PlaylistListResource(Resource):
 
 
 class PlaylistCreateResource(Resource):
-    @roles_required('user')
     @marshal_with(playlist_fields)
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('name', type=str, required=True,
                             help='Name of the playlist is required')
-        parser.add_argument('songs', type=list, required=True,
+        parser.add_argument('songs', type=list, default=[],
                             help='List of song IDs is required')
         args = parser.parse_args()
 
         playlist_name = args['name']
         song_ids = args['songs']
 
+        # Check if a playlist with the same name already exists
         existing_playlist = Playlist.query.filter_by(
             name=playlist_name).first()
         if existing_playlist:
             return {'error': f'Playlist with name {playlist_name} already exists.'}, 400
 
-        songs = Song.query.filter(Song.id.in_(song_ids)).all()
-        if len(songs) != len(song_ids):
-            return {'error': 'One or more provided song IDs are invalid.'}, 400
+        # If songs are provided, retrieve the songs based on the provided song IDs
+        if song_ids:
+            songs = Song.query.filter(Song.id.in_(song_ids)).all()
+            if len(songs) != len(song_ids):
+                return {'error': 'One or more provided song IDs are invalid.'}, 400
+        else:
+            songs = []
 
-        # Get the current user ID
-        user_id = getattr(auth_required.current_user, 'id', None)
+        # Get the current user ID using authentication logic
+        user_id = current_user.id
         if not user_id:
             return {'error': 'User ID not found'}, 401
 
+        # Create a new playlist
         new_playlist = Playlist(name=playlist_name, user_id=user_id)
 
-        # Link the playlist with songs through the SongsLiked table
+        # Link the playlist with songs through the PlaylistSong table
         for song in songs:
-            new_playlist.songs.append(song)
+            playlist_song = PlaylistSong(song=song)
+            new_playlist.songs.append(playlist_song)
 
+        # Add the new playlist to the database
         db.session.add(new_playlist)
         db.session.commit()
 
