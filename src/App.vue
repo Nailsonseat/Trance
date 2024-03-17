2f5e5169febd7edaf4aa5c0c6095adf188fd2659
nailsonseat <aadarshv3@gmail.com>
2024-03-17 14:46:17 +0530

    Logic for role wise navigation and prevention

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ export default {
       showDisplayNavbar: false,
       showAdminButton: false,
       showLogoutButton: false,
+      role: null,
     };
   },
   methods: {
@@ -15,46 +16,87 @@ export default {
     },
     logout() {
       const path = 'http://127.0.0.1:5000/';
-      axios.get(path + 'logout-user')
+      axios.get(path + 'logout-user', { headers: { 'auth-token': localStorage.getItem('auth-token') } })
         .then(response => {
           console.log('Logout successful:', response.data);
           // Clear auth token and role from local storage
 
-          const tokenKey = 'auth-token';
-          const roleKey = 'role';
-          localStorage.removeItem(tokenKey);
-          localStorage.removeItem(roleKey);
-          // Redirect to the login page or perform other actions based on the response
-          this.$router.push('/login-user');
+          this.removeRoleFromLocalStorage()
+            .then(() => {
+              // Redirect to the login page or perform other actions based on the response
+              this.$router.push('/login-user');
+            });
         })
         .catch(error => {
           console.error('Logout error:', error.response.data.error);
         });
     },
+    checkAuthorization(to) {
+      // If the route requires creator role and the user is not a creator, redirect to unauthorized page
+      if (to.meta.requiresCreator && this.role !== 'creator') {
+        this.$router.push('/unauthorized');
+        return false;
+      }
+
+      // If the route requires user role and the user is not a user, redirect to unauthorized page
+      // if (to.meta.requiresUser && this.role !== 'user') {
+      //   this.$router.push('/unauthorized');
+      //   return false;
+      // }
+
+      return true; // User is authorized to access the route
+    },
     shouldDisplayNavbarOnRoute(route) {
-      //return route.name !== 'UserHome'
+      // return route.name !== 'UserHome'
       return true;
     },
     shouldDisplayAdminButton(route) {
       return route.name === 'UserAuth' || route.name === 'CreatorAuth';
     },
     shouldDisplayLogoutButton(route) {
-      return route.name !== 'UserAuth' && route.name !== 'CreatorAuth';
-    }
+      return !(route.name === 'UserAuth' || route.name === 'CreatorAuth' || route.name === 'AdminAuth');
+    },
+    fetchRoleFromLocalStorage() {
+      return new Promise(resolve => {
+        const role = localStorage.getItem('role');
+        this.role = role;
+        resolve();
+      });
+    },
+    removeRoleFromLocalStorage() {
+      return new Promise(resolve => {
+        // Remove the role from localStorage
+        localStorage.removeItem('role');
+        localStorage.removeItem('auth-token');
+        this.role = null;
+        // Resolve the promise
+        resolve();
+      });
+    },
   },
   watch: {
     // Watch for changes in the route and update shouldDisplayNavbar accordingly
-    $route(to, from) {
+    async $route(to, from) {
+      await this.fetchRoleFromLocalStorage();
+      this.checkAuthorization(to);
       this.showDisplayNavbar = this.shouldDisplayNavbarOnRoute(to);
       this.showLogoutButton = this.shouldDisplayLogoutButton(to);
       this.showAdminButton = this.shouldDisplayAdminButton(to);
+
+      if (this.role === 'creator' && (this.$route.name === 'CreatorAuth' || this.$route.name === 'UserAuth')) {
+        this.$router.push('/home-creator');
+      } else if (this.role === 'user' && (this.$route.name === 'CreatorAuth' || this.$route.name === 'UserAuth')) {
+        this.$router.push('/home-user');
+      }
     },
   },
   created() {
     // Set the initial value of shouldDisplayNavbar based on the current route
+
     this.showDisplayNavbar = this.shouldDisplayNavbarOnRoute(this.$route);
     this.showDisplayLogoutButton = this.shouldDisplayLogoutButton(this.$route);
     this.showAdminButton = this.shouldDisplayAdminButton(this.$route);
+
   },
 };
 </script>
@@ -70,7 +112,7 @@ export default {
       <router-link class="nav-link" to="/">Home</router-link>
       <router-link class="nav-link" to="/login-user">Login</router-link>
       <router-link class="nav-link" to="/home-user">User Home</router-link>
-      <router-link class="nav-link" to="/home-creator">Creator Home</router-link>
+      <router-link class="nav-link" style="margin-right: auto;" to="/home-creator">Creator Home</router-link>
       <button v-if="showAdminButton" @click="goToAdminAuth()" class="logout-btn">Admin</button>
       <button v-if="showLogoutButton" @click="logout()" class="logout-btn">Logout</button>
       <!-- Add more links as needed -->
@@ -128,6 +170,8 @@ html {
 .nav-link {
   color: white;
   align-self: center;
+  justify-self: start;
+  justify-content: start;
   text-decoration: none;
   margin-left: 3rem;
 }
@@ -139,7 +183,6 @@ html {
   color: white;
   border: none;
   cursor: pointer;
-  margin-left: auto;
   margin-right: 3%;
   font-size: 1em;
 }
